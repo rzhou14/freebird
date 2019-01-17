74c886aed739966f4e093e38b996240c64a01aac
Rachel Zhou <rzhou14@illinois.edu>
2019-01-17 17:48:36 -0600

    dependency

--- R/ihmm.R
+++ R/ihmm.R
@@ -14,24 +14,10 @@
 #'
 #' @export
 #'
-#' @importFrom lars lars
-#' @importFrom scalreg lars
-#' @importFrom scalreg scalreg
 #' @importFrom stats lm pnorm
 #' @examples
 #' ## Example to be added here
-#'library(MASS)
-#library(flare)
-rm(list=ls())
-n=30
-p=50
-q=2
-G = mvrnorm(n, rep(0,p), diag(p))
-S = as.matrix(mvrnorm(n, rep(0,q), diag(q)))
-Y = as.matrix(rnorm(n))
 
-out = ihmm(Y,G,S, mediation_setting = 'complete', tuning_method = 'uniform', lam_list = 0.2)
-out = ihmm(Y,G,S, mediation_setting = 'incomplete')
 
 #' # c.f. test_data.R file
 ihmm <- function(Y, G, S, mediation_setting = 'incomplete', tuning_method = 'aic', lam_list = NA,
@@ -61,6 +47,7 @@ ihmm <- function(Y, G, S, mediation_setting = 'incomplete', tuning_method = 'aic
 
     sigma_SS_hat_inverse = solve(sigma_SS_hat)
 
+    library(scalreg)
     result_scalreg = scalreg(X,Y)
     alpha_hat = result_scalreg$co
     sigma1_hat = result_scalreg$hsigma
