bdd0b0c09b4e3a75a701ccd931c1b476ecb6824e
Rachel Zhou <rzhou14@illinois.edu>
2019-01-22 12:28:55 -0600

    Modified init see function def

--- src/init.c
+++ src/init.c
@@ -1,40 +1,22 @@
-#include <R.h>
-#include <Rinternals.h>
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
-/* FIXME:
- Check these declarations against the C/Fortran source code.
- */
+/* FIXME: 
+   Check these declarations against the C/Fortran source code.
+*/
 
-/* .Call calls */
-extern SEXP dantzig_ladm_scr(SEXP, SEXP, SEXP, SEXP, SEXP,
-                             SEXP, SEXP, SEXP, SEXP, SEXP,
-                             SEXP, SEXP, SEXP, SEXP, SEXP,
-                             SEXP, SEXP, SEXP, SEXP, SEXP);
+/* .C calls */
+extern void slim_dantzig_ladm_scr(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void slim_dantzig_ladm_scr2(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 
-extern SEXP slim_dantzig_ladm_scr(SEXP, SEXP, SEXP, SEXP, SEXP,
-                                  SEXP, SEXP, SEXP, SEXP, SEXP,
-                                  SEXP, SEXP, SEXP, SEXP, SEXP,
-                                  SEXP, SEXP, SEXP, SEXP, SEXP,
-                                  SEXP);
-
-
-extern SEXP slim_dantzig_ladm_scr2(SEXP, SEXP, SEXP, SEXP, SEXP,
-                                  SEXP, SEXP, SEXP, SEXP, SEXP,
-                                  SEXP, SEXP, SEXP, SEXP, SEXP,
-                                  SEXP, SEXP, SEXP);
-
-static const R_CallMethodDef CallEntries[] = {
-  {"dantzig_ladm_scr",            (DL_FUNC) &dantzig_ladm_scr,           20},
-  {"slim_dantzig_ladm_scr",    (DL_FUNC) &slim_dantzig_ladm_scr,    21},
-  {"slim_dantzig_ladm_scr2",    (DL_FUNC) &slim_dantzig_ladm_scr,    18},
-  {NULL, NULL, 0}
+static const R_CMethodDef CEntries[] = {
+    {"slim_dantzig_ladm_scr",  (DL_FUNC) &slim_dantzig_ladm_scr,  21},
+    {"slim_dantzig_ladm_scr2", (DL_FUNC) &slim_dantzig_ladm_scr2, 18},
+    {NULL, NULL, 0}
 };
 
-void R_init_sitmo(DllInfo *dll)
+void R_init_freebird(DllInfo *dll)
 {
-  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
-  R_useDynamicSymbols(dll, FALSE);
+    R_registerRoutines(dll, CEntries, NULL, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
 }
-
